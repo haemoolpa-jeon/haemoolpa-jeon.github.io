7109f02e4bfe25915798239a228d013225b9e813
haemoolpa-jeon <42135681+haemoolpa-jeon@users.noreply.github.com>
2019-05-17 20:23:29 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];
+var colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
 var donutOptions = {
-  cutoutPercentage: 85, 
-  legend: {position:'bottom', 
-    labels:{pointStyle:'circle',
-    usePointStyle:true}
+  cutoutPercentage: 85,
+  legend: {
+    position: 'bottom',
+    labels: {
+      pointStyle: 'circle',
+      usePointStyle: true
+    }
   }
 };
 var chDonutData1 = {
   labels: ['Bootstrap', 'Popper', 'Other'],
   datasets: [
     {
-      backgroundColor: colors.slice(0,3),
+      backgroundColor: colors.slice(0, 3),
       borderWidth: 0,
       data: [74, 11, 40]
     }
   ]
 };
 var chDonut1 = document.getElementById("chDonut1");
-  if (chDonut1) {
+if (chDonut1) {
   new Chart(chDonut1, {
     type: 'pie',
     data: chDonutData1,
     options: donutOptions
   });
 }
+
+
+/* ---------- JS for circular progress bar at Home ---------- */
