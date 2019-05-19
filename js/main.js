a5bd3f5591f2c817860378f67a1e6fac7ee41b2a
haemoolpa-jeon <42135681+haemoolpa-jeon@users.noreply.github.com>
2019-05-19 14:02:39 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,3 +30,43 @@ if (chDonut1) {
 
 
 /* ---------- JS for circular progress bar at Home ---------- */
+var counter = document.getElementById('counter').getContext('2d');
+var no = 0; // Starting Point
+var pointToFill = 4.72;  //Point from where you want to fill the circle
+var cw = counter.canvas.width;  //Return canvas width
+var ch = counter.canvas.height; //Return canvas height
+var diff;   // find the different between current value (no) and trageted value (100)
+
+function fillCounter() {
+  diff = ((no / 100) * Math.PI * 2 * 10);
+
+  counter.clearRect(0, 0, cw, ch);   // Clear canvas every time when function is call
+
+  counter.lineWidth = 77;     // size of stroke
+
+  counter.fillStyle = '#fff';     // color that you want to fill in counter/circle
+
+  counter.strokeStyle = '#F5E0A9';    // Stroke Color
+
+  counter.textAlign = 'center';
+
+  counter.font = "300px monospace";    //set font size and face
+
+  counter.fillText(no + '%', 520, 520);       //fillText(text,x,y);
+
+  counter.beginPath();
+  counter.arc(500, 500, 450, pointToFill, diff / 10 + pointToFill);    //arc(x,y,radius,start,stop)
+
+  counter.stroke();   // to fill stroke
+
+  // now add condition
+
+  if (no >= 80) {
+    clearTimeout(fill);     //fill is a variable that call the function fillcounter()
+  }
+  no++;
+}
+
+//now call the function
+
+var fill = setInterval(fillCounter, 50);     //call the fillCounter function after every 50MS
