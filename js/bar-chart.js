e43ccd032694ff9a61712477b6ab1b6fbbe10908
haemoolpa-jeon <42135681+haemoolpa-jeon@users.noreply.github.com>
2019-05-26 02:19:55 +1000

    Update bar-chart.js

--- js/bar-chart.js
+++ js/bar-chart.js
@@ -2,11 +2,11 @@
 new Chart(document.getElementById("bar-chart"), {
     type: 'bar',
     data: {
-      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday"],
+      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
       datasets: [
         {
           backgroundColor: ["#fff", "#fff","#fff","#fff","#fff","#fff","#fff","#fff","#fff"],
-          data: [11, 30, 10, 9, 13, 10, 12, 44]
+          data: [62.6, 58.4, 72.5, 63, 0, 0, 0, 0]
         }
       ]
     },
